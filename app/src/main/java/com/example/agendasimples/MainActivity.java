5d15d6add3a2a0ab63d5749d17d27ee4e0783495
Wesley <wesleyvitor37417@gmail.com>
2021-12-06 14:40:44 -0300

    recuperar compromissos contidos em uma string

--- app/src/main/java/com/example/agendasimples/MainActivity.java
+++ app/src/main/java/com/example/agendasimples/MainActivity.java
@@ -19,25 +19,42 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Compromisso> compromissos = new ArrayList<Compromisso>();
         CompromissoAdapter compromissoAdapter;
 
-        Compromisso c1 = new Compromisso("10 de Dezembro", "Farmácia", 0);
-        Compromisso c2 = new Compromisso("02 de Janeiro", "Dentista", 1);
-        Compromisso c3 = new Compromisso("23 de Outubro", "Supermercado", 0);
-        Compromisso c4 = new Compromisso("10 de Dezembro", "Farmácia", 2);
-        Compromisso c5 = new Compromisso("02 de Janeiro", "Dentista", 3);
-        Compromisso c6 = new Compromisso("23 de Outubro", "Supermercado", 1);
+//        Compromisso c1 = new Compromisso("10 de Dezembro", "Farmácia", 0);
+//        Compromisso c2 = new Compromisso("02 de Janeiro", "Dentista", 1);
+//        Compromisso c3 = new Compromisso("23 de Outubro", "Supermercado", 0);
+//        Compromisso c4 = new Compromisso("10 de Dezembro", "Farmácia", 2);
+//        Compromisso c5 = new Compromisso("02 de Janeiro", "Dentista", 3);
+//        Compromisso c6 = new Compromisso("23 de Outubro", "Supermercado", 1);
+//
+//        compromissos.add(c1);
+//        compromissos.add(c2);
+//        compromissos.add(c3);
+//        compromissos.add(c4);
+//        compromissos.add(c5);
+//        compromissos.add(c6);
 
-        compromissos.add(c1);
-        compromissos.add(c2);
-        compromissos.add(c3);
-        compromissos.add(c4);
-        compromissos.add(c5);
-        compromissos.add(c6);
+        String compromissosS = "#\n" +
+                "data: 16 de Dezembro\n" +
+                "titulo: Farmácia\n" +
+                "idColor: 0\n" +
+                "\n" +
+                "#\n" +
+                "data: 12 de Dezembro\n" +
+                "titulo: Supermercado\n" +
+                "idColor: 1\n";
+
+        compromissos = Compromisso.getCompromissosFromText(compromissosS);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rv.setLayoutManager(linearLayoutManager);
 
         compromissoAdapter = new CompromissoAdapter(compromissos);
         rv.setAdapter(compromissoAdapter);
+
+
+
+
+
 
     }
 }
