25f0ba20797a4c1e11400e0006101eb41cb3b462
Wesley <wesleyvitor37417@gmail.com>
2021-12-08 10:35:51 -0300

    ...

--- app/src/main/java/com/example/agendasimples/MainActivity.java
+++ app/src/main/java/com/example/agendasimples/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView emptyComrpomissosText = findViewById(R.id.empty_compromissos_text);
         RecyclerView rv = findViewById(R.id.compromissos_recycle);
         CompromissoAdapter compromissoAdapter;
         FloatingActionButton addButton = findViewById(R.id.add_button);
@@ -37,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         compromissoAdapter = new CompromissoAdapter(compromissos);
         rv.setAdapter(compromissoAdapter);
 
+        if(compromissos.isEmpty()){
+            emptyComrpomissosText.setVisibility(View.VISIBLE);
+        }else{
+            emptyComrpomissosText.setVisibility(View.INVISIBLE);
+        }
 
         addButton.setOnClickListener(view -> addCompromissoActivity());
 
