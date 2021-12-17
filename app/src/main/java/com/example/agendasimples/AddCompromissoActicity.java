dfda80af1933e214f54627e5b239889737039c19
Wesley <wesleyvitor37417@gmail.com>
2021-12-16 22:24:30 -0300

    Testes com a formatação da data, assim como a alteração do valor desta

--- app/src/main/java/com/example/agendasimples/AddCompromissoActicity.java
+++ app/src/main/java/com/example/agendasimples/AddCompromissoActicity.java
@@ -18,6 +18,7 @@ import java.util.Locale;
 public class AddCompromissoActicity extends AppCompatActivity {
 
     TextView dateText;
+    Calendar dateCalendar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +26,10 @@ public class AddCompromissoActicity extends AppCompatActivity {
         setContentView(R.layout.activity_add_compromisso_acticity);
 
         dateText = findViewById(R.id.data_text);
+        dateCalendar = Calendar.getInstance();
         Button changeDateButton = findViewById(R.id.change_date_button);
+
+        dateText.setText(getStringFromCalendar(dateCalendar));
 
         changeDateButton.setOnClickListener(view -> calendarDialog());
 
@@ -39,20 +43,28 @@ public class AddCompromissoActicity extends AppCompatActivity {
         View testeView = layoutInflater.inflate(R.layout.date_picker_layout, null);
 
         DatePicker datePicker = (DatePicker) testeView.findViewById(R.id.date);
-        ///////datePicker.updateDate();
+
+        //atualiza a data do datePicker para a contida no dateCalendar
+        datePicker.updateDate(dateCalendar.get(Calendar.YEAR),
+                dateCalendar.get(Calendar.MONTH),
+                dateCalendar.get(Calendar.DAY_OF_MONTH));
         
         builder.setPositiveButton("Ok", (dialogInterface, i) -> {
-
-            Locale local = new Locale("pt","BR");
-            DateFormat dateFormat= new SimpleDateFormat("dd 'de' MMM ',' yyyy",local);
-
-            Calendar calendar = getCalendarFromDatePicker(datePicker);
-
-            dateText.setText(dateFormat.format(calendar.getTime()));
+            dateCalendar = getCalendarFromDatePicker(datePicker);
+            dateText.setText(getStringFromCalendar(dateCalendar));
         });
 
         builder.setView(testeView);
         builder.show();
+
+    }
+
+    public static String getStringFromCalendar(Calendar calendar){
+
+        Locale locale = new Locale("pt", "BR");
+        DateFormat dateFormat = new SimpleDateFormat("dd 'de' MMM ',' yyyy", locale);
+
+        return dateFormat.format(calendar.getTime());
 
     }
 
